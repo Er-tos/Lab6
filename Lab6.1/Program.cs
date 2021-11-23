e42eba50e27ee8bfa1a7cfcad28dcfe65bcae0da
Анатолий Ершов <a.v.ershov@sgugit.ru>
2021-11-23 22:15:42 +0700

    Добавьте файлы проекта.

---
+++ Lab6.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение, в котором программа определит самое длинное слово");
+            string input = Console.ReadLine();
+            string[] str = input.Split(new Char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            int max = 0, index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length > max)
+                {
+                    max = str[i].Length;
+                    index = i;
+                }
+            }
+            Console.Write("Самое длинное слово в предложении - {0}", str[index]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab6.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите словосочетание. Программа опредлит, является ли оно палиндромом.");
+            string userString = Console.ReadLine().ToLower();
+            string[] userStringArray = userString.Split();
+            string straightString = String.Join("", userStringArray);
+            string[] backStringArray = new string[1];
+            for (int i = straightString.Length - 1; i >= 0; i--)
+            {
+                backStringArray[0] = backStringArray[0] + straightString[i];
+            }
+            string backString = String.Join("", backStringArray);
+            Console.WriteLine();
+            int flag = String.Compare(straightString, backString);
+            if (flag == 0)
+            {
+                Console.WriteLine("Это словосочетание - палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Это словосочетание - не палиндром");
+            }
+            Console.ReadKey();
+        }
+    }
+}
